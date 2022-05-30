c13cbf1a772b95e2b29ef2653175f6ae40f31f01
GolubeykovAnton <ds6580@yandex.ru>
2022-05-30 20:41:45 +0300

    Initial Commit

---
+++ twitterLike/twitterLikeApp.swift
@@ -0,0 +1,17 @@
+//
+//  twitterLikeApp.swift
+//  twitterLike
+//
+//  Created by Антон Голубейков on 30.05.2022.
+//
+
+import SwiftUI
+
+@main
+struct twitterLikeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
